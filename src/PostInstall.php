b41a65dc69139fa077a4c9b8c5335035dd1ad9a1
Thibault Truffert <thibaulttruffert@hotmail.com>
2018-05-26 20:58:42 +0200

    fix post install files

--- src/PostInstall.php
+++ src/PostInstall.php
@@ -17,27 +17,27 @@ class PostInstall
         
         mkdir($templateFolder);
         
-        $layout = '<?php
-/* @var $this League\Plates\Template\Template */
-?>
-
-<html>
-    <head>
-        <title>Test Plates</title>
-    </head>
-    <body>
-        <?php echo $this->section(\'body\'); ?>
-    </body>
-</html>';
+        $layout = "<?php\n
+/* @var $this League\Plates\Template\Template */\n
+?>\n
+\n
+<html>\n
+    <head>\n
+        <title>Test Plates</title>\n
+    </head>\n
+    <body>\n
+        <?php echo \$this->section(\'body\'); ?>\n
+    </body>\n
+</html>";
         
-        $template = '<?php
-/* @var $this League\Plates\Template\Template */
-
-$this->layout(\'layout\');
-?>
-<?php $this->start(\'body\'); ?>
-Hello Plates !!
-<?php $this->stop();';
+        $template = "<?php\n
+/* @var \$this League\Plates\Template\Template */\n
+\n
+\$this->layout(\'layout\');\n
+?>\n
+<?php \$this->start(\'body\'); ?>\n
+Hello Plates !!\n
+<?php \$this->stop();";
         
         file_put_contents($templateFolder.'/'.$layoutFileName, $layout);
         file_put_contents($templateFolder.'/'.$templateFileName, $template);
